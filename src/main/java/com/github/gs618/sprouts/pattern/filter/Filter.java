0dcfb47d44f8583f0e0f31cd3a889b4a19bb5dff
gaosongdalian@sina.com <gaosongdlaian@sina.com>
2021-01-22 14:40:30 +0800

    0.0.3

--- src/main/java/com/github/gs618/sprouts/pattern/filter/Filter.java
+++ src/main/java/com/github/gs618/sprouts/pattern/filter/Filter.java
@@ -16,4 +16,31 @@ public interface Filter<I, O> {
 	 */
 	void doFilter(I input, O output, FilterChain<I, O> filterChain);
 
+	/**
+	 * if the filter should run
+	 *
+	 * @param input input
+	 * @param output output
+	 * @return true: the filter runs
+	 * 				false: the filter is bypassed
+	 */
+	default boolean shouldFilter(I input, O output) {
+		return true;
+	}
+
+	/**
+	 * This method will run until all of the downstream filter calls back
+	 *
+	 * @param input input
+	 * @param output output
+	 */
+	default void postFilter(I input, O output) {}
+
+	/**
+	 * Runs before filter method
+	 *
+	 * @param input input
+	 * @param output output
+	 */
+	default void preFilter(I input, O output) {}
 }
--- src/main/java/com/github/gs618/sprouts/pattern/filter/FilterChain.java
+++ src/main/java/com/github/gs618/sprouts/pattern/filter/FilterChain.java
@@ -14,27 +14,64 @@ public class FilterChain<I, O> {
 
 	private Iterator<Map.Entry<Integer, Filter<I, O>>> iterator;
 
+	private Exception exception = null;
+
+	private boolean success = true;
+
+	/**
+	 *
+	 * @return Null: No exception happens during the filterChain runs, the flag success should be true
+	 * 		Not null: there must be an exception thrown out during the process, the flag success should be false
+	 */
+	public Exception getException() {
+		return exception;
+	}
+
+	/**
+	 *
+	 * @return true: success
+	 * 				false: failure
+	 */
+	public boolean isSuccess() {
+		return success;
+	}
+
 	/**
 	 * Invoke filter
-	 * @param input input to filter
+	 *
+	 * @param input  input to filter
 	 * @param output output from filter
 	 */
 	public void doFilter(I input, O output) {
-		if(iterator.hasNext()) {
+		if (iterator.hasNext()) {
 			Filter<I, O> filter = iterator.next().getValue();
-			filter.doFilter(input, output, this);
+			if (filter.shouldFilter(input, output)) {
+				try {
+					filter.preFilter(input, output);
+					filter.doFilter(input, output, this);
+				} catch (Exception e) {
+					// doFilter is a recursion so the exception will not be spread
+					exception = e;
+					success = false;
+				} finally {
+					filter.postFilter(input, output);
+				}
+			} else {
+				doFilter(input, output);
+			}
 		}
 	}
 
 	/**
 	 * add a filter
+	 *
 	 * @param filter Filter
 	 * @return FilterChain
 	 */
 	public FilterChain<I, O> addFilter(Filter<I, O> filter) {
 		FilterPriority annotation = filter.getClass().getAnnotation(FilterPriority.class);
 		int priority;
-		if(Objects.nonNull(annotation)) {
+		if (Objects.nonNull(annotation)) {
 			priority = annotation.value();
 		} else {
 			priority = Filter.DEFAULT_PRIORITY;
@@ -46,7 +83,7 @@ public class FilterChain<I, O> {
 	 * add a filter
 	 *
 	 * @param priority the smaller of the number, the higher of the priority
-	 * @param filter	Filter
+	 * @param filter   Filter
 	 * @return FilterChain
 	 */
 	public FilterChain<I, O> addFilter(Integer priority, Filter<I, O> filter) {
@@ -56,6 +93,7 @@ public class FilterChain<I, O> {
 
 	/**
 	 * reset iterator
+	 *
 	 * @return FilterChain
 	 */
 	public FilterChain<I, O> reset() {
