9af739ffc9c4e0897ca4c19d729b8692689c6436
sgao <sgao@beckman.com>
2021-04-25 11:05:20 +0800

    Release 0.0.5: if filter skipped and ignore rest filters, then FilterChain failed.

--- src/test/java/test/com/github/gs618/sprouts/pattern/filter/FilterTest.java
+++ src/test/java/test/com/github/gs618/sprouts/pattern/filter/FilterTest.java
@@ -2,8 +2,11 @@ package test.com.github.gs618.sprouts.pattern.filter;
 
 import com.github.gs618.sprouts.pattern.filter.FilterChain;
 import org.junit.Test;
+import test.com.github.gs618.sprouts.pattern.filter.step.ReturnTestStep1;
 import test.com.github.gs618.sprouts.pattern.filter.step.Step1Filter1Option1;
+import test.com.github.gs618.sprouts.pattern.filter.step.Step1Filter1Option2;
 import test.com.github.gs618.sprouts.pattern.filter.step.Step2Filter1Option1;
+import test.com.github.gs618.sprouts.pattern.filter.step.Step2Filter1Option2;
 
 import java.util.List;
 
@@ -25,5 +28,17 @@ public class FilterTest {
 		Step2Filter1Option1 step2Filter1Option1 = new Step2Filter1Option1();
 		integerListFilterChain.addFilter(1, step2Filter1Option1).addFilter(10, step1Filter1Option1);
 		integerListFilterChain.doFilter(null, null);
+	}
+
+	@Test
+	public void filterChainReturnTest() {
+		FilterChain<Integer, List<Integer>> integerListFilterChain = new FilterChain<>();
+		integerListFilterChain.addFilter(new Step1Filter1Option1());
+		integerListFilterChain.addFilter(new Step1Filter1Option2());
+		integerListFilterChain.addFilter(new ReturnTestStep1());
+		integerListFilterChain.addFilter(new Step2Filter1Option1());
+		integerListFilterChain.addFilter(new Step2Filter1Option2());
+		integerListFilterChain.doFilter(null, null);
+		System.out.println(integerListFilterChain.isSuccess());
 	}
 }
